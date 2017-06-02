d52641de081d75eda6e010faa7a60117cdacf929
Francis Ohemeng-Adu <francis_adu@yahoo.com>
2017-06-02 04:51:13 +0000

    updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com'
+    :enable_starttls_auto => true
   }
 
   # Settings specified here will take precedence over those in config/application.rb.
